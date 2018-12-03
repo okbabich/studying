c890f8e55e874b3b95d3bb264f00c1684ea1427a
okbabich <ksyukha-babich@yandex.ru>
2018-12-03 17:58:47 +0200

    Point and Main classes without get/set

--- sandbox/src/main/java/com/qa/studying/sandbox/Main.java
+++ sandbox/src/main/java/com/qa/studying/sandbox/Main.java
@@ -1,24 +1,17 @@
 package com.qa.studying.sandbox;
 
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Point p1 = new Point(2.04, 78);
         Point p2 = new Point(3, 7.78);
 
-        System.out.println("Distance between p1 with coordinates: (" + p1.getX() + ";" + p1.getY() + ")" + " and p2 with" +
-                " coordinates: (" + p2.getX() + ";" + p2.getY() + ") = " + distance(p1, p2));
-
-        System.out.println("Method in a class result: Distance between p1 with coordinates: (" + p1.getX() + ";" + p1.getY() + ")" + " and p2 with" +
-                " coordinates: (" + p2.getX() + ";" + p2.getY() + ") = " + Point.distance(p1,p2));
+        System.out.println("Distance between p1 with coordinates: (" + p1.x + ";" + p1.y + ")" + " and p2 with coordinates: (" + p2.x + ";" + p2.y + ") = " + distance(p1, p2));
 
     }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt(Math.pow((p2.getX() - p1.getX()), 2) + Math.pow((p2.getY() - p1.getY()), 2));
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) +  Math.pow((p2.y - p1.y), 2));
 
     }
 
 }
-
